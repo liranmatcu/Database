de691278ffac3ddd775d1ca195135b7052c644ef
liranmatcu <l.ma@tcu.edu>
2022-09-22 23:27:21 -0500

    Create index.php

---
+++ PHP-Nginx/scripts/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en-ca">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="./mycss.css" type="text/css">
+    <title>COSC 30603 DBMS</title>
+
+    <h1>Database Access Menu</h1>
+    <p>Please make a choice by clicking on one the links listed below</p>
+
+    <nav>
+        <ul>
+            <li><a href="test-connection.php">Test Connection</a></li>
+            <li><a href="query.php">Query the Employee table of the Company database</a></li>
+        </ul>
+    </nav>
+
+</body>
+</html>
