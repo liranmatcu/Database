8aa651f75f5d2c7347c5b5dbb0f16cb7b3190521
Liran Ma <l.ma@tcu.edu>
2022-10-17 17:08:00 -0500

    Update 3-order-by-limit.sql

--- SQL-Demo/DQL/3-order-by-limit.sql
+++ SQL-Demo/DQL/3-order-by-limit.sql
@@ -1,5 +1,17 @@
 USE COMPANY;
 
+/*
+ The ORDER BY keyword is used to sort the
+ result-set in ascending or descending order.
+
+ The ORDER BY keyword sorts the records in ascending order by default.
+ To sort the records in descending order, use the DESC keyword.
+
+ SELECT column1, column2, ...
+ FROM table_name
+ ORDER BY column1, column2, ... ASC|DESC;
+ */
+
 # ORDER BY c1, c2 ...
 SELECT Fname AS "First Name", Lname "Last Name", Salary
 FROM EMPLOYEE
@@ -24,8 +36,19 @@ FROM EMPLOYEE
 WHERE Salary BETWEEN 10000 AND 40000
 ORDER BY Salary DESC, Last_Name DESC;
 
-# LIMIT [number1,] number2
-# Typically, in the end of a SQL statement
+/*
+ The LIMIT clause is used to specify the number of records to return.
+
+ The LIMIT clause is useful on large tables with thousands of records.
+ Returning a large number of records can impact performance.
+
+ LIMIT [number1,] number2
+ n1 : starting index
+ n2 : number of record/data to show
+
+ Typically, in the end of a SQL statement
+ */
+
 SELECT Fname AS "First Name", Lname "Last Name", Salary
 FROM EMPLOYEE
 WHERE Salary BETWEEN 10000 AND 40000
