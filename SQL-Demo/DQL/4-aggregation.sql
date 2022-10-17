933acb50c954957d888cc9e0dedee24c43a0e647
liranmatcu <l.ma@tcu.edu>
2022-10-16 22:18:37 -0500

    Update 4-aggregation.sql

--- SQL-Demo/DQL/4-aggregation.sql
+++ SQL-Demo/DQL/4-aggregation.sql
@@ -2,22 +2,50 @@
 -- SQL aggregate keywords: Group by, Having
 
 USE COMPANY;
-SELECT * FROM EMPLOYEE;
+
+# Sum() and Avg()
+SELECT SUM(Salary) AS "Total Salary" FROM EMPLOYEE;
+SELECT AVG(Salary) "Average Salary" FROM EMPLOYEE;
+
+# Min(), Max()
+SELECT MIN(Salary) AS "Minimum Salary",
+       MAX(Salary) "Maximum Salary"
+FROM EMPLOYEE;
+
+SELECT max(Lname), min(Fname)
+FROM EMPLOYEE;
+
 
 # COUNT()
-SELECT COUNT(*) FROM EMPLOYEE;
-SELECT COUNT(1) FROM EMPLOYEE;
+SELECT COUNT(Ssn)
+FROM EMPLOYEE;
 
-SELECT COUNT(Ssn) FROM EMPLOYEE;
-SELECT COUNT(Super_ssn) FROM EMPLOYEE;
+SELECT count(Salary), count(2*Salary)
+FROM EMPLOYEE;
 
+SELECT COUNT(*)
+FROM EMPLOYEE;
 
-# Avg(), Min(), Max()
-SELECT AVG(Salary) FROM EMPLOYEE;
+SELECT COUNT(1)
+FROM EMPLOYEE;
+-- * and 1 return the same results
 
-SELECT MIN(Salary) AS "Minimum Salary" FROM EMPLOYEE;
+-- Would 2 change anything?
+SELECT COUNT(2)
+FROM EMPLOYEE;
 
-SELECT MAX(Salary) "Maximum Salary" FROM EMPLOYEE;
+-- Does each column return the same result in counting?
+SELECT COUNT(Super_ssn)
+FROM EMPLOYEE;
+
+-- NULL does not count
+
+-- What does the following do?
+SELECT AVG(Salary) * (
+    SELECT count(1)
+    FROM EMPLOYEE)
+FROM EMPLOYEE;
+
 
 # Group By and Having
 -- Get the average salary by supervisor
