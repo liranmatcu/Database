a45108078e8b4bf87f3b4bcdc35c421fd806b956
Liran Ma <l.ma@tcu.edu>
2022-10-18 09:52:17 -0500

    Update 4-aggregation-keywords.sql

--- SQL-Demo/DQL/4-aggregation-keywords.sql
+++ SQL-Demo/DQL/4-aggregation-keywords.sql
@@ -1,56 +1,12 @@
--- SQL aggregate functions: Count(), Sum(), Avg(), Min(), Max()
--- SQL aggregate keywords: Group by, Having
+/*
+    SQL aggregate keywords: 
+    Group by, Having
+*/
+-- 
 
 USE COMPANY;
 
-# Sum() and Avg()
-SELECT SUM(Salary) AS "Total Salary" FROM EMPLOYEE;
-SELECT AVG(Salary) "Average Salary" FROM EMPLOYEE;
 
-# Min(), Max()
-SELECT MIN(Salary) AS "Minimum Salary",
-       MAX(Salary) "Maximum Salary"
-FROM EMPLOYEE;
-
-SELECT max(Lname), min(Fname)
-FROM EMPLOYEE;
-
-
-# COUNT(): count the number of occurrence
-SELECT COUNT(Ssn)
-FROM EMPLOYEE;
-
-SELECT count(Salary), count(2*Salary)
-FROM EMPLOYEE;
-
-SELECT COUNT(*)
-FROM EMPLOYEE;
-
-SELECT COUNT(1)
-FROM EMPLOYEE;
--- * and 1 return the same results
-
--- Would 2 change anything?
-SELECT COUNT(2)
-FROM EMPLOYEE;
-
--- Does each column return the same result in counting?
-SELECT COUNT(Super_ssn)
-FROM EMPLOYEE;
-
--- NULL does not count!
-
--- What does the following do?
-SELECT avg(Salary), sum(Salary)/count(Salary)
-FROM EMPLOYEE;
-
-SELECT AVG(Salary) * (
-    SELECT count(1)
-    FROM EMPLOYEE)
-FROM EMPLOYEE;
-
-
-# Group By and Having
 /*
  The GROUP BY statement groups rows that
  have the same values into summary rows.
