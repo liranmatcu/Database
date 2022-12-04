cfab18c7217547e2ffb520296634d57972715a57
liranmatcu <l.ma@tcu.edu>
2022-12-04 12:44:11 -0600

    Update index-basics.sql

--- SQL-Demo/Index/index-basics.sql
+++ SQL-Demo/Index/index-basics.sql
@@ -1,7 +1,11 @@
 USE demo;
 
-# Implicit index creation on table creation by default
-# Indexes will be created for PK, FK and Unique columns
+/*
+ Implicit index creation:
+
+ Indexes will be created for PK, FK and UNIQUE columns
+ by default at the time of table creation.
+ */
 DROP TABLE IF EXISTS dept_index_demo;
 CREATE TABLE IF NOT EXISTS dept_index_demo(
     dept_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -19,10 +23,28 @@ CREATE TABLE IF NOT EXISTS emp_index_demo(
 
 SHOW INDEXES FROM emp_index_demo;
 
-# Explicit index creation:
-# Create indexes for a table at the time of creation
 /*
+ Explicit index creation:
+ Specify indexes for a table at the time of creation
+
+ Syntax:
  [UNIQUE | FULLTEXT | SPATIAL] INDEX index_name (column_list)
+
+ * INDEX (or KEY) refers to a normal non-unique index.
+
+ * UNIQUE refers to an index where all rows of the index must be unique.
+ That is, the same row may not have identical non-NULL values
+ for all columns in this index as another row.
+
+ * PRIMARY acts exactly like a UNIQUE index, except that it is always named 'PRIMARY',
+ and there may be only one on a table
+
+ * FULLTEXT indexes are only useful for full text searches,
+ and their behaviour differs significantly between database systems.
+
+ * SPATIAL indexes are for indexing geo-objects - shapes.
+ The spatial index makes it possible to efficiently search for objects that overlap in space.
+
  */
 DROP TABLE IF EXISTS book;
 CREATE TABLE IF NOT EXISTS book(
@@ -30,7 +52,7 @@ CREATE TABLE IF NOT EXISTS book(
     book_name VARCHAR(100),
     book_author VARCHAR(100),
     book_comments VARCHAR(300),
-    INDEX idx_name(book_name) # A simple index on book_name
+    INDEX idx_name(book_name) # A normal non-unique index on book_name
 ) ;
 SHOW INDEXES FROM book;
 
